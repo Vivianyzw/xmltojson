ba9c89261d16b0f36217229786d5ddafc0a73d09
Vivian <vivianziweiyang@gmail.com>
2018-03-20 15:14:00 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,9 @@ import xml.etree.ElementTree as ET
 import cv2
 import numpy as np
 import json
+import copy
 
+classes = ["car"]
 
 class ANNO:
     def __init__(self):
@@ -22,26 +24,27 @@ class ANNO:
         self.category = {}
 
     def init_index(self):
-        self.dictionary = {}
-        self.dictionary["images"] = []
-        self.dictionary["annotations"] = []
-        self.dictionary["categories"] = []
+        self.dictionary = {"images": [], "annotations": [], "categories": []}
 
-    def input_data(self, dict):
-        self.filename = dict["imgid"] + ".jpg"
-        self.height = dict["height"]
-        self.width = dict["width"]
-        self.imgid = dict["imgid"]
-        self.segmentation = dict["segmentation"]
-        self.area = dict["area"]
-        self.iscrowd = dict["iscrowd"]
-        self.bbox = dict["bbox"]
-        self.catid = dict["catid"]
-        self.annid = dict["annid"]
+    def input_data(self, dict, annid):
+        self.filename = str(dict["images"]["imgid"]) + ".jpg"
+        self.height = dict["images"]["height"]
+        self.width = dict["images"]["width"]
+        self.imgid = dict["images"]["imgid"]
         self.image = {"file_name": self.filename, "height": self.height, "width": self.width, "id": self.imgid}
-        self.annotation = {"segmentation": self.segmentation, "area": self.area, "iscrowd": self.iscrowd, "image_id":
+
+        for dic in dict["annotations"]:
+            annid += 1
+            self.segmentation = dic["segmentation"]
+            self.area = dic["area"]
+            self.iscrowd = dic["iscrowd"]
+            self.bbox = dic["bbox"]
+            self.catid = dic["category_id"]
+            self.annid = annid
+            self.annotation = {"segmentation": self.segmentation, "area": self.area, "iscrowd": self.iscrowd, "image_id":
             self.imgid, "bbox": self.bbox, "category_id": self.catid, "id": self.annid}
         self.make_dict()
+        return annid
 
     def make_dict(self):
         self.dictionary["images"].append(self.image)
@@ -53,17 +56,23 @@ class ANNO:
 
 
 def convert_annotation(image_id):
+    segmentations = []
+    classall = []
     in_file = open('xml/%s.xml' % (image_id))
     tree = ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
     width = int(size.find('width').text)
     height = int(size.find('height').text)
-    var = 1
-    i = 1
-    segmentation = []
-
     for obj in root.iter('object'):
+        i = 0
+        var = 1
+        segmentation = []
+        cls = obj.find('name').text
+        if cls not in classes:
+            continue
+        cls_id = classes.index(cls) + 1
+        classall.append(cls_id)
         polygon = obj.find('polygon')
         while var == 1:
             polygon_index = "point" + str(i)
@@ -76,50 +85,58 @@ def convert_annotation(image_id):
                 for index in co_list:
                     x = int(index)
                     segmentation.append(x)
-    return width, height, segmentation
+        segmentations.append(segmentation)
+    return width, height, segmentations, classall
 
 
-def get_bbox(segmentation):
-    x = segmentation[::2]
-    y = segmentation[1::2]
-    xmin = min(x)
-    xmax = max(x)
-    ymin = min(y)
-    ymax = max(y)
-    width = xmax - xmin
-    height = ymax - ymin
-    return [xmin, ymin, width, height]
+def get_bbox(segmentations):
+    boxes = []
+    for i in range(len(segmentations)):
+        segmentation = copy.deepcopy(segmentations[i])
+        x = segmentation[::2]
+        y = segmentation[1::2]
+        xmin = min(x)
+        xmax = max(x)
+        ymin = min(y)
+        ymax = max(y)
+        width = xmax - xmin
+        height = ymax - ymin
+        box = [xmin, ymin, width, height]
+        boxes.append(box)
+    return boxes
 
 
-def get_area(segmentation):
-    xarray = segmentation[::2]
-    yarray = segmentation[1::2]
-    length = len(xarray)
-    listall = []
-    for i in range(0, length):
-        ll = [[xarray[i], yarray[i]]]
-        listall.append(ll)
-    array_value = np.array(listall)
-    contours = []
-    contours.append(array_value)
-    area = cv2.contourArea(contours[0])
-    return area
+def get_area(segmentations):
+    areas = []
+    for i in range(len(segmentations)):
+        segmentation = copy.deepcopy(segmentations[i])
+
+        xarray = segmentation[::2]
+        yarray = segmentation[1::2]
+        length = len(xarray)
+        listall = []
+        for i in range(0, length):
+            ll = [[xarray[i], yarray[i]]]
+            listall.append(ll)
+        array_value = np.array(listall)
+        contours = []
+        contours.append(array_value)
+        area = cv2.contourArea(contours[0])
+        areas.append(area)
+    return areas
 
 
-def save_json(height, width, imgid, segmentation, area, iscrowd, bbox, catid, annid, image_id):
-    annotation = {}
-    annotation["height"] = height
-    annotation["width"] = width
-    annotation["imgid"] = imgid
-    annotation["segmentation"] = [segmentation]
-    annotation["area"] = area
-    annotation["iscrowd"] = iscrowd
-    annotation["bbox"] = bbox
-    annotation["catid"] = catid
-    annotation["annid"] = annid
+def save_json(height, width, imgid, segmentations, areas, iscrowd, bboxes, catids, annid, image_id):
+    dictionary = {"annotations": []}
+    image = {"height": height, "width": width, "imgid": imgid}
+    dictionary["images"] = image
+    for i in range(len(segmentations)):
+        annotation = {"segmentation": [segmentations[i]], "area": areas[i], "iscrowd": iscrowd, "image_id": imgid,
+                      "bbox": bboxes[i], "category_id": catids[i], "id": annid}
+        dictionary["annotations"].append(annotation)
 
     json_file = open('json/%s.json' % (image_id), 'w')
-    json.dump(annotation, json_file)
+    json.dump(dictionary, json_file)
     json_file.close()
 
 
@@ -130,23 +147,24 @@ def read_json(json_name):
 
 
 image_ids = open('ls.txt').read().strip().split()
-catid = 1
+print image_ids
 for image_id in image_ids:
-    w, h, segment = convert_annotation(image_id)
-    bbox = get_bbox(segment)
-    area = get_area(segment)
-    save_json(h, w, image_id, segment, area, 0, bbox, catid, image_id, image_id)
+    width, height, segmentations, classall = convert_annotation(image_id)
+    bboxes = get_bbox(segmentations)
+    areas = get_area(segmentations)
+    save_json(height, width, image_id, segmentations, areas, 0, bboxes, classall, image_id, image_id)
 
 
 voc = ANNO()
 
 voc.init_index()
 voc.add_category()
+annid = 1
 
 for image_id in image_ids:
     diction = read_json('json/%s.json' % (image_id))
-    voc.input_data(diction)
-
-json_file = open('result.json', 'w')
+    annid = voc.input_data(diction, annid)
+    print annid
+json_file = open('results.json', 'w')
 json.dump(voc.dictionary, json_file)
 json_file.close()
